a8b498da9163f69e15752c3e3406067641a9e15d
Josh Knox <jknox0610@gmail.com>
2021-10-14 14:41:15 -0400

    setup vca.javascript

--- public/behavior.js
+++ public/behavior.js
@@ -1,16 +1,37 @@
-window.console.log(window)
-window.console.log(window.document)
-window.console.log(window.document.documentElement)
-window.console.log(window.document.body)
+// get the URL
+// ?name=Tucker+Knox&breed=Labradoodle&spade=on
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
-const headerElement = window.document.querySelector("header")
-const itemElementC = window.document.querySelector(".c")
-const itemElementO = window.document.querySelector(".o")
-const itemElementD = window.document.querySelector(".d")
-const itemElementE = window.document.querySelector(".e")
+// get the value for ?name= in the URL
+const name = urlParams.get('name')
 
-window.console.log(headerElement)
-window.console.log(itemElementC)
-window.console.log(itemElementO)
-window.console.log(itemElementD)
-window.console.log(itemElementE)
+// get the value for ?breed= in the URL
+const breed = urlParams.get('breed')
+
+// get the value for ?spade= in the URL
+const spade = urlParams.get('spade')
+
+// get the name element
+const nameElement = document.querySelector("[id=result-name]");
+
+// get the breed element
+const breedElement = document.querySelector("[id=result-breed]");
+
+// get the spade element
+const spadeElement = document.querySelector("[id=result-spade]");
+
+// get the headline element
+const headlineElement = document.querySelector("[id=result-headline]");
+
+// make the headline element say the name
+headlineElement.innerText = name
+
+// make the name element say the name
+nameElement.innerText = name
+
+// make the breed element say the breed
+breedElement.innerText = breed
+
+// make the spade element say the spade
+spadeElement.innerText = spade
