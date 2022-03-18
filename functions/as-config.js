14e3e9a8e17b418a5ef05ae6bd13b6d2c91827fb
Josh Knox <jknox0610@gmail.com>
2022-03-18 12:36:25 -0400

    Add ActivityPub functionality

---
+++ functions/as-config.js
@@ -0,0 +1,11 @@
+
+
+const localDomain = 'https://joshknox.rocks';
+const localActor = `${localDomain}/as/actor`;
+
+module.exports = {
+    config: {
+        localDomain,
+        localActor,
+    }
+};
